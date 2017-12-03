573790dc3535f1a1a514c382994d93e19cb3c43e
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-12-03 00:50:38 -0500

    Rearrange the tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -22,31 +22,10 @@ class Datainer(Metainer):
 d1 = Lict(0.1, __name__='d1')
 d2 = Lict(0.2, __name__='d2')
 d3 = Lict(0.3, __name__='d3')
-
-def test_metainer():
-    m = Metainer() # `m.__dict__` does not exist without subclassing `Metainer`
-
-    with pytest.raises(AttributeError) as e:
-        m.set('x', d1, kind='coord')
-    assert str(e.value) == "'Metainer' object has no attribute 'x'"
-
-    with pytest.raises(AttributeError) as e:
-        m.x = d1
-    assert str(e.value) == "'Metainer' object has no attribute 'x'"
-
-def test_set():
-    d = Datainer()
-    d.set('x', d1, kind='coord')
-    d.set('y', d2, kind='coord')
-    d.set('z', d3, kind='coord')
-    assert d._metainer == [
-        [d1, ('name', 'x'), ('kind', 'coord')],
-        [d2, ('name', 'y'), ('kind', 'coord')],
-        [d3, ('name', 'z'), ('kind', 'coord')],
-    ]
+d  = Datainer()
 
 def test_setattr():
-    d = Datainer()
+    global d
     d.x = d1
     d.y = d2
     d.z = d3
@@ -56,11 +35,40 @@ def test_setattr():
         [d3, ('name', 'z')],
     ]
 
+    # `m.__dict__` does not exist without subclassing `Metainer`
+    with pytest.raises(AttributeError) as e:
+        Metainer().x = d1
+    assert str(e.value) == "'Metainer' object has no attribute 'x'"
+
+def test_set():
+    global d
+    d.set('x', d1, kind='coord')
+    d.set('y', d2, kind='coord')
+    d.set('z', d3, kind='coord')
+    assert d._metainer == [
+        [d1, ('name', 'x')],
+        [d2, ('name', 'y')],
+        [d3, ('name', 'z')],
+        [d1, ('name', 'x'), ('kind', 'coord')],
+        [d2, ('name', 'y'), ('kind', 'coord')],
+        [d3, ('name', 'z'), ('kind', 'coord')],
+    ]
+
+    # `m.__dict__` does not exist without subclassing `Metainer`
+    with pytest.raises(AttributeError) as e:
+        Metainer().set('x', d1, kind='coord')
+    assert str(e.value) == "'Metainer' object has no attribute 'x'"
+
+def test_getattr():
+    assert d.x == d1
+    assert d.y == d2
+    assert d.z == d3
+
+    with pytest.raises(AttributeError) as e:
+        a = d.a
+    assert str(e.value) == "'Datainer' object has no attribute 'a'"
+
 def test_get():
-    d = Datainer()
-    d.x = d1
-    d.y = d2
-    d.z = d3
     assert d.get('x') == d1
     assert d.get('y') == d2
     assert d.get('z') == d3
@@ -69,14 +77,5 @@ def test_get():
         a = d.get('a')
     assert str(e.value) == "'Datainer' object has no attribute 'a'"
 
-    assert d.get('a', None) == None
+    assert d.get('a', 1) == 1
     assert d.get('a', 1, 2) == (1, 2)
-
-def test_getattr():
-    d = Datainer()
-    d.x = d1
-    d.y = d2
-    d.z = d3
-    assert d.x == d1
-    assert d.y == d2
-    assert d.z == d3
