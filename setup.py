8f18e21456bf356bea9f6e546a6f21b3049323ba
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2017-12-03 20:18:32 -0500

    Release 0.1.1
    Use metadata as mount points based on a customizable mounting list.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='metainer',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/hallmarksrc/metainer',
     author='Chi-kwan Chan',
     author_email='ckchan@cfa.harvard.edu',
